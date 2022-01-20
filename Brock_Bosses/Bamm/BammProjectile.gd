7986aac010b1cd0b3ada9c91f2749c7df4a29654
yooozn <yooozn@github.com>
2022-01-20 11:12:09 -0800

    added all particles to attacks

--- Brock_Bosses/Bamm/BammProjectile.gd
+++ Brock_Bosses/Bamm/BammProjectile.gd
@@ -7,7 +7,7 @@ func _ready():
 	rng.randomize()
 	var startScale = rng.randf_range(0.2,4)
 	var endScale = rng.randf_range(.2,4)
-	$Tween.interpolate_property(self, "position:x", position.x, position.x - 2000, rng.randf_range(.6, 1.5),Tween.TRANS_LINEAR,Tween.EASE_IN)
+	$Tween.interpolate_property(self, "position:x", position.x, position.x + 2000, rng.randf_range(.6, 1.5),Tween.TRANS_LINEAR,Tween.EASE_IN)
 	$Tween.interpolate_property(self, "scale", Vector2(startScale,startScale), Vector2(endScale,endScale),rng.randf_range(.1,4.0),Tween.TRANS_LINEAR,Tween.EASE_IN)
 	$Tween.start()
 	yield(get_tree().create_timer(3),"timeout")
