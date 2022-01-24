5e724a6bea257d95d0db4c088630a587219e8417
yooozn <yooozn@github.com>
2022-01-24 14:16:30 -0800

    Added save information comments

--- SaveandLoading/SaveAndLoad.gd
+++ SaveandLoading/SaveAndLoad.gd
@@ -5,6 +5,8 @@ var room = "res://Spark Forest.tscn"
 var playerPos = Vector2(0,0)
 
 func _Save():
+	#Variable data which is a dictionary to store all data. All values will be changed throughout different scenes, and scripts
+	#and saved via the SaveandLoad._Save() method(Can be used globally). This method saves the following data shown below.
 	var data = {
 		"1" : room,
 		"2" : playerPos,
@@ -12,6 +14,7 @@ func _Save():
 		"4" : "four",
 		"5" : "five"
 	}
+	#Opens file, unless there is an error.
 	var file = File.new()
 	var error = file.open(save_path,File.WRITE)
 	if error == OK: 
@@ -19,6 +22,7 @@ func _Save():
 		file.close()
 
 func _newGame():
+	#Starts a fresh dictionary data, and wipes old data
 	var data = {
 		"1" : "res://Spark Forest.tscn",
 		"2" : "two",
@@ -31,9 +35,11 @@ func _newGame():
 	if error == OK: 
 		file.store_var(data)
 		file.close()
+		#Transitions to spark forest when called(level 1)
 	get_tree().change_scene("res://Spark Forest.tscn")
 	
 func _Load():
+	#Loads previously saved data
 	var file = File.new()
 	if file.file_exists(save_path):
 		var error = file.open(save_path, File.READ)
